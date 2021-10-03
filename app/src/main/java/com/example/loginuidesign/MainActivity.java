56432c491990a191907797a042451ecdd602d200
wayosu <wahyusman10@gmail.com>
2021-10-04 01:25:42 +0800

    final commit

---
+++ app/src/main/java/com/example/loginuidesign/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.loginuidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
